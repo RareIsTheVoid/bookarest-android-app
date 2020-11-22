4028401a5ccbab3ff7869b21a0dcb9c8279345a0
RareIsTheVoid <davidr9o@yahoo.ro>
2020-11-22 13:04:59 +0200

    started home fragment

--- app/src/main/java/com/example/bookarest/fragment_home.java
+++ app/src/main/java/com/example/bookarest/fragment_home.java
@@ -7,13 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class fragment_home extends Fragment {
+
+    ImageView img;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        img = v.findViewById(R.id.img_home_currently_reading);
+        img.setClipToOutline(true);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return v;
+
     }
 }
